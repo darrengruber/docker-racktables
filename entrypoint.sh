0910ad230980ce5ea27e907206dca0ebb4be779a
Darren Gruber <dgruber@gmail.com>
2015-01-24 16:46:20 +0000

    Move to apache

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
-export > /etc/envvars
-exec /usr/sbin/runsvdir-start
+cat <<EOF > /usr/local/share/RackTables/wwwroot/inc/secret.php
+<?php
+\$pdo_dsn = 'mysql:host=mysql;dbname=$MYSQL_ENV_MYSQL_DATABASE';
+\$db_username = '$MYSQL_ENV_MYSQL_USER';
+\$db_password = '$MYSQL_ENV_MYSQL_PASSWORD';
+#\$user_auth_src = 'database';
+#\$require_local_account = TRUE;
+EOF
+
+chown www-data: /usr/local/share/RackTables/wwwroot/inc/secret.php
+chmod 400 /usr/local/share/RackTables/wwwroot/inc/secret.php
+
+exec apache2-foreground
